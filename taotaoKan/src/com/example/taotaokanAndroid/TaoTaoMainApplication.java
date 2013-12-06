287652f94abd8284ae4ec2406cff3d9ac57bdfbd
oday0311 <oday0311@hotmail.com>
2013-12-06 14:41:23 +0800

    df

--- taotaoKan/src/com/example/taotaokanAndroid/TaoTaoMainApplication.java
+++ taotaoKan/src/com/example/taotaokanAndroid/TaoTaoMainApplication.java
@@ -35,30 +35,65 @@ public class TaoTaoMainApplication extends Application {
             R.drawable.bg_source_12,
     };
 
+    public String[] categoryStrings = {
+            "19.9包邮",
+            "29.9包邮",
+            "9.9包邮",
+            "母婴儿童",
+            "精美女包",
 
+            "美容护肤",
+            "精美DIY",
+            "潮流女装",
+            "时尚女鞋",
+            "数码配件",
+
+            "美食特产",
+            "精美礼物",
+            "家具建材",
+            "",
+            "个性男装",
+
+
+            "最新商品",
+            "户外运动",
+            "",
+            "",
+            "",
+
+            "",
+            "",
+            "",
+            "",
+            "",
+
+
+    };
     public int[] categoryImages = {
             R.drawable.ca_19,
             R.drawable.ca_29,
             R.drawable.ca_9,
             R.drawable.ca_baby,
             R.drawable.ca_bag,
+
             R.drawable.ca_cosmetic,
             R.drawable.ca_diy,
             R.drawable.ca_fashion,
             R.drawable.ca_female_shoe,
             R.drawable.ca_figure,
-            R.drawable.ca_food,
 
+            R.drawable.ca_food,
             R.drawable.ca_gift,
             R.drawable.ca_home,
             R.drawable.ca_jewelry,
             R.drawable.ca_male,
+
             R.drawable.ca_new,
             R.drawable.ca_outdoors,
             R.drawable.ca_redwine,
-
             R.drawable.ca_select,
             R.drawable.ca_shoe,
+
             R.drawable.ca_skirt,
             R.drawable.ca_socks,
             R.drawable.ca_sport,
